63d07708254b7d9710a3befd20726820ebe9e21f
Eduardo Straub <eduardo.straub1@gmail.com>
2018-12-10 20:57:34 -0300

    Se completa el ejercicio 1.

--- Ejercicio1/ejercicio1.rb
+++ Ejercicio1/ejercicio1.rb
@@ -6,17 +6,17 @@
 # index.html y pondrá como párrafo cada uno de los strings recibidos.
 
 def html_strings(str1, str2)
-  File.open('ejercio1.html', 'a') { |page| page.puts str1 + ' <br>' + str2 + ' <br>' }
+  File.open('ejercio1.html', 'a') { |page| page.puts '<p>' + str1 + '</p>' + '<p>' + str2 + '</p>' }
 end
 
-html_strings('Buenas', 'tardes')
+html_strings('Buenas', 'Chao')
 
 # 2. Crear un método similar al anterior, que además pueda recibir un arreglo. Si
 # el arreglo no está vacío, agregar debajo de los párrafos una lista ordenada
 # con cada uno de los elementos.
 
 def html_array(arr)
-  File.open('ejercio1.html', 'a') { |page| arr.each { |e| page.puts e.to_s + ' <br>' } } unless arr.nil?
+  File.open('ejercio1.html', 'a') { |page| arr.each { |e| page.puts '<p>' + e.to_s + '</p>' } } unless arr.nil?
 end
 
 arr = []
@@ -24,6 +24,23 @@ for i in 0..10 do
   arr.push(i)
 end
 html_array(arr)
+
+# 3. Crear un tercer método que además pueda recibir un color. Agregar color de fondo
+# a los párrafos.
+
+def color(color)
+  file = File.open('ejercio1.html', 'a')
+  file.puts '<br>'
+  file.puts '<head>'
+  file.puts '<style>'
+  file.puts 'p {'
+  file.puts 'background-color: ' + color + ';'
+  file.puts '}'
+  file.puts '</style>'
+  file.puts '<head>'
+end
+
+color('yellow')
 
 # rubocop:enable AsciiComments
 # rubocop:enable LineLength
