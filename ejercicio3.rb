91d4e5cc523f1021ae17fdefd9ffe99db8485be9
Eduardo Straub <eduardo.straub1@gmail.com>
2018-12-16 23:33:10 -0300

    No me funciona todavía.

--- ejercicio3.rb
+++ ejercicio3.rb
@@ -1,20 +1,25 @@
 # rubocop:disable LineLength
 
 def total_palabras(archivo)
-  arreglo = leer_arachivo(archivo)
+  arreglo = leer_archivo(archivo)
   counter = arreglo.inject(0) { |acumulador, linea| linea.split.length + acumulador }
   counter
 end
 
-def leer_arachivo(archivo)
+def leer_archivo(archivo)
   file = File.open(archivo, 'r')
   arreglo = file.readlines
   file.close
   arreglo
 end
 
-
+def cantidad_palabra_repetidas(archivo, palabra)
+  arreglo = leer_archivo(archivo)
+  counter = arreglo.inject(0) { |acumulador, linea| linea.split.length + acumulador if acumulador == palabra }
+  counter
+end
 
 puts total_palabras('peliculas.txt')
+puts cantidad_palabra_repetidas('peliculas.txt', 'Guerra')
 
 # rubocop:enable LineLength
