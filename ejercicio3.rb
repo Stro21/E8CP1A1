9e4dc2ff0a30bc74789b21326bd13f5085c97d36
Eduardo Straub <eduardo.straub1@gmail.com>
2018-12-16 23:13:19 -0300

    Se agrega el método leer archivo.

--- ejercicio3.rb
+++ ejercicio3.rb
@@ -1,12 +1,19 @@
 # rubocop:disable LineLength
 
 def total_palabras(archivo)
-  file = File.open(archivo, 'r')
-  arreglo = file.readlines
-  file.close
+  arreglo = leer_arachivo(archivo)
   counter = arreglo.inject(0) { |acumulador, linea| linea.split.length + acumulador }
   counter
 end
+
+def leer_arachivo(archivo)
+  file = File.open(archivo, 'r')
+  arreglo = file.readlines
+  file.close
+  arreglo
+end
+
+
 
 puts total_palabras('peliculas.txt')
 
