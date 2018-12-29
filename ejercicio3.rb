e68fbea4276589ac63385201848be982aa142ef3
Eduardo Straub <eduardo.straub1@gmail.com>
2018-12-29 13:42:02 -0300

    Change language from spanish to english.

--- ejercicio3.rb
+++ ejercicio3.rb
@@ -1,25 +1,19 @@
 # rubocop:disable LineLength
 
-# def total_palabras(archivo)
-#   arreglo = leer_archivo(archivo)
-#   counter = arreglo.inject(0) { |acumulador, linea| linea.split.length + acumulador }
-#   counter
-# end
-
-def leer_archivo(archivo)
-  arreglo = File.open(archivo, 'r') { |file| file.readlines.map(&:chomp) }
+def read_file(txt)
+  data = File.open(txt, 'r') { |file| file.readlines.map(&:chomp) }
   array = []
-  arreglo.each { |arr| array.push(arr.split(' ')) }
+  data.each { |arr| array.push(arr.split(' ')) }
   array.flatten
 end
 
 # def cantidad_palabra_repetidas(archivo, palabra)
-#   arreglo = leer_archivo(archivo)
+#   arreglo = read_file(archivo)
 #   counter = arreglo.inject(0) { |acumulador, linea| linea.split.length + acumulador if acumulador == palabra }
 #   counter
 # end
 
-arreglo = leer_archivo('peliculas.txt')
-print "#{arreglo}\n"
+words = read_file('peliculas.txt')
+print "#{words}\n"
 
 # rubocop:enable LineLength
