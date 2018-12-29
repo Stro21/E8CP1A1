5f1131a529c7815eb931d57ad958673443fd473b
Eduardo Straub <eduardo.straub1@gmail.com>
2018-12-29 13:37:57 -0300

    Deja todas las palabras separadas en un solo array.

--- ejercicio3.rb
+++ ejercicio3.rb
@@ -1,27 +1,25 @@
 # rubocop:disable LineLength
 
-def total_palabras(archivo)
-  arreglo = leer_archivo(archivo)
-  counter = arreglo.inject(0) { |acumulador, linea| linea.split.length + acumulador }
-  counter
-end
+# def total_palabras(archivo)
+#   arreglo = leer_archivo(archivo)
+#   counter = arreglo.inject(0) { |acumulador, linea| linea.split.length + acumulador }
+#   counter
+# end
 
 def leer_archivo(archivo)
   arreglo = File.open(archivo, 'r') { |file| file.readlines.map(&:chomp) }
-  print "#{arreglo}\n"
   array = []
-  arreglo.each { |arr| array << arr.split(' ') }
-  print "#{array}\n"
-  array
+  arreglo.each { |arr| array.push(arr.split(' ')) }
+  array.flatten
 end
 
-def cantidad_palabra_repetidas(archivo, palabra)
-  arreglo = leer_archivo(archivo)
-  counter = arreglo.inject(0) { |acumulador, linea| linea.split.length + acumulador if acumulador == palabra }
-  counter
-end
+# def cantidad_palabra_repetidas(archivo, palabra)
+#   arreglo = leer_archivo(archivo)
+#   counter = arreglo.inject(0) { |acumulador, linea| linea.split.length + acumulador if acumulador == palabra }
+#   counter
+# end
 
-puts total_palabras('peliculas.txt')
-puts cantidad_palabra_repetidas('peliculas.txt', 'Guerra')
+arreglo = leer_archivo('peliculas.txt')
+print "#{arreglo}\n"
 
 # rubocop:enable LineLength
