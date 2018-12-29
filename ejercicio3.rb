2dab6b2819ecd021c6868567518b17945a679beb
Eduardo Straub <eduardo.straub1@gmail.com>
2018-12-29 14:09:57 -0300

    Se completo el ejercicio 3.

--- ejercicio3.rb
+++ ejercicio3.rb
@@ -7,13 +7,36 @@ def read_file(txt)
   array.flatten
 end
 
-# def cantidad_palabra_repetidas(archivo, palabra)
-#   arreglo = read_file(archivo)
-#   counter = arreglo.inject(0) { |acumulador, linea| linea.split.length + acumulador if acumulador == palabra }
-#   counter
-# end
+def total_words(file)
+  words = read_file(file)
+  puts "La cantidad total de palabras es #{words.length}."
+end
 
-words = read_file('peliculas.txt')
-print "#{words}\n"
+def word_hash(words)
+  hash = {}
+  words.each do |word|
+    if hash[word].nil?
+      hash[word] = 1
+    else
+      hash[word] += 1
+    end
+  end
+  hash
+end
+
+def reapeat_words(file, word)
+  words = read_file(file)
+  hash = word_hash(words)
+  if hash[word].nil?
+    puts "La palabra no esta en el texto #{file}"
+  elsif hash[word] > 1
+    puts "La palabra #{word} esta repetida #{hash[word]} veces en el texto #{file}"
+  else
+    puts "Sola hay una palabra #{word} en el texto #{file}"
+  end
+end
+
+total_words('peliculas.txt')
+reapeat_words('peliculas.txt', 'Sith')
 
 # rubocop:enable LineLength
